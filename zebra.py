bb391421daf0c5c9cb5e022f914d1afe67231c37
Fazzie <1240419984@qq.com>
2021-03-20 14:44:10 +0800

    first commit

---
+++ zebra.py
@@ -0,0 +1,7 @@
+# Developer：Fazzie
+# Time: 2021/3/2014:38
+# File name: zebra.py
+# Development environment: Anaconda Python
+
+from kanren import run, eq, membero, var, conde        # kanren一个描述性Python逻辑编程系统
+from kanren.core import lall   # lall包用于定义规则

ed4d6460ba0a796d74ea94b2d14f50e4a6055dd9
Fazzie <1240419984@qq.com>
2021-06-15 20:42:50 +0800

    DQN

---
+++ Deep-Q-Learning/main.py
@@ -0,0 +1,451 @@
+# Developer：Fazzie
+# Time: 2021/6/1411:01
+# File name: main.py
+# Development environment: Anaconda Python
+
+# 导入相关包
+import os
+import random
+import numpy as np
+from Maze import Maze
+from Runner import Runner
+from QRobot import QRobot
+from ReplayDataSet import ReplayDataSet
+from torch_py.MinDQNRobot import MinDQNRobot as TorchRobot  # PyTorch版本
+import matplotlib.pyplot as plt
+from torch_py.QNetwork import QNetwork
+import torch.nn.functional as F
+import torch
+from torch import optim
+
+'''
+""" 创建迷宫并展示 """
+maze = Maze(maze_size=10) # 随机生成迷宫
+print(maze)
+
+
+rewards = [] # 记录每走一步的奖励值
+actions = [] # 记录每走一步的移动方向
+
+# 循环、随机移动机器人10次，记录下奖励
+for i in range(10):
+    valid_actions = maze.can_move_actions(maze.sense_robot())
+    action = random.choice(valid_actions)
+    rewards.append(maze.move_robot(action))
+    actions.append(action)
+
+print("the history of rewards:", rewards)
+print("the actions", actions)
+
+# 输出机器人最后的位置
+print("the end position of robot:", maze.sense_robot())
+
+# 打印迷宫，观察机器人位置
+print(maze)
+'''
+
+# 机器人移动方向
+move_map = {
+    'u': (-1, 0),  # up
+    'r': (0, +1),  # right
+    'd': (+1, 0),  # down
+    'l': (0, -1),  # left
+}
+
+
+# 迷宫路径搜索树
+class SearchTree(object):
+
+    def __init__(self, loc=(), action='', parent=None):
+        """
+        初始化搜索树节点对象
+        :param loc: 新节点的机器人所处位置
+        :param action: 新节点的对应的移动方向
+        :param parent: 新节点的父辈节点
+        """
+
+        self.loc = loc  # 当前节点位置
+        self.to_this_action = action  # 到达当前节点的动作
+        self.parent = parent  # 当前节点的父节点
+        self.children = []  # 当前节点的子节点
+
+    def add_child(self, child):
+        """
+        添加子节点
+        :param child:待添加的子节点
+        """
+        self.children.append(child)
+
+    def is_leaf(self):
+        """
+        判断当前节点是否是叶子节点
+        """
+        return len(self.children) == 0
+
+
+def expand(maze, is_visit_m, node):
+    """
+    拓展叶子节点，即为当前的叶子节点添加执行合法动作后到达的子节点
+    :param maze: 迷宫对象
+    :param is_visit_m: 记录迷宫每个位置是否访问的矩阵
+    :param node: 待拓展的叶子节点
+    """
+    can_move = maze.can_move_actions(node.loc)
+    for a in can_move:
+        new_loc = tuple(node.loc[i] + move_map[a][i] for i in range(2))
+        if not is_visit_m[new_loc]:
+            child = SearchTree(loc=new_loc, action=a, parent=node)
+            node.add_child(child)
+
+
+def back_propagation(node):
+    """
+    回溯并记录节点路径
+    :param node: 待回溯节点
+    :return: 回溯路径
+    """
+    path = []
+    while node.parent is not None:
+        path.insert(0, node.to_this_action)
+        node = node.parent
+    return path
+
+
+def breadth_first_search(maze):
+    """
+    对迷宫进行广度优先搜索
+    :param maze: 待搜索的maze对象
+    """
+    start = maze.sense_robot()
+    root = SearchTree(loc=start)
+    queue = [root]  # 节点队列，用于层次遍历
+    h, w, _ = maze.maze_data.shape
+    is_visit_m = np.zeros((h, w), dtype=np.int)  # 标记迷宫的各个位置是否被访问过
+    path = []  # 记录路径
+    while True:
+        current_node = queue[0]
+        is_visit_m[current_node.loc] = 1  # 标记当前节点位置已访问
+
+        if current_node.loc == maze.destination:  # 到达目标点
+            path = back_propagation(current_node)
+            break
+
+        if current_node.is_leaf():
+            expand(maze, is_visit_m, current_node)
+
+        # 入队
+        for child in current_node.children:
+            queue.append(child)
+
+        # 出队
+        queue.pop(0)
+
+    return path
+
+
+'''
+maze = Maze(maze_size=10)
+height, width, _ = maze.maze_data.shape
+
+path_1 = breadth_first_search(maze)
+print("搜索出的路径：", path_1)
+
+for action in path_1:
+    maze.move_robot(action)
+
+if maze.sense_robot() == maze.destination:
+    print("恭喜你，到达了目标点")
+
+print(maze)
+'''
+
+
+def my_search(maze):
+    """
+    对迷宫实现深度优先搜索算法
+    :param maze: 迷宫对象
+    :return :到达目标点的路径 如：["u","u","r",...]
+    """
+
+    start = maze.sense_robot()
+    root = SearchTree(loc=start)
+    h, w, _ = maze.maze_data.shape
+    is_visit_m = np.zeros((h, w), dtype=np.int)  # 标记迷宫的各个位置是否被访问过
+    path = []  # 记录路径
+    path = dfs(maze, is_visit_m, root)
+
+    return path
+
+
+def dfs(maze, is_visit_m, current_node):
+    if current_node.loc == maze.destination:  # 到达目标点
+        path = back_propagation(current_node)
+        return path
+
+    elif current_node.is_leaf():
+        expand(maze, is_visit_m, current_node)
+        for node in current_node.children:
+            is_visit_m[node.loc] = 1
+            path = dfs(maze, is_visit_m, node)
+            if not path == None:
+                return path
+
+    else:
+        return None
+
+'''
+maze = Maze(maze_size=10)  # 从文件生成迷宫
+
+path_2 = my_search(maze)
+print("搜索出的路径：", path_2)
+
+for action in path_2:
+    maze.move_robot(action)
+
+if maze.sense_robot() == maze.destination:
+    print("恭喜你，到达了目标点")
+'''
+
+'''
+maze = Maze(maze_size=5) # 随机生成迷宫
+
+robot = QRobot(maze) # 记得将 maze 变量修改为你创建迷宫的变量名
+
+action, reward = robot.train_update() # QLearning 算法一次Q值迭代和动作选择
+
+print("the choosed action: ", action)
+print("the returned reward: ", action)
+'''
+
+"""  Qlearning 算法相关参数： """
+
+'''
+epoch = 10  # 训练轮数
+epsilon0 = 0.5  # 初始探索概率
+alpha = 0.5  # 公式中的 ⍺
+gamma = 0.9  # 公式中的 γ
+maze_size = 5  # 迷宫size
+
+""" 使用 QLearning 算法训练过程 """
+
+g = Maze(maze_size=maze_size)
+r = QRobot(g, alpha=alpha, epsilon0=epsilon0, gamma=gamma)
+
+runner = Runner(r)
+runner.run_training(epoch, training_per_epoch=int(maze_size * maze_size * 1.5))
+
+# 生成训练过程的gif图, 建议下载到本地查看；也可以注释该行代码，加快运行速度。
+runner.generate_gif(filename="results/size5.gif")
+
+runner.plot_results() # 输出训练结果，可根据该结果对您的机器人进行分析。
+'''
+'''
+test_memory = ReplayDataSet(max_size=1e3) # 初始化并设定最大容量
+actions = ['u', 'r', 'd', 'l']
+test_memory.add((0,1), actions.index("r"), -10, (0,1), 1)  # 添加一条数据（state, action_index, reward, next_state）
+print(test_memory.random_sample(1)) # 从中随机抽取一条（因为只有一条数据）
+'''
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"  # 允许重复载入lib文件
+
+maze = Maze(maze_size=5)
+
+
+"""选择keras版本或者torch版本的机器人, MinRobot是尽量选择reward值最小的动作，对象初始化过程中修改了maze的reward参数"""
+# robot = KerasRobot(maze=maze)
+robot = TorchRobot(maze=maze)
+
+print(robot.maze.reward) # 输出最小值选择策略的reward值
+
+"""开启金手指，获取全图视野"""
+robot.memory.build_full_view(maze=maze) #
+
+"""training by runner"""
+runner = Runner(robot=robot)
+runner.run_training(training_epoch=10, training_per_epoch=75)
+
+"""Test Robot"""
+robot.reset()
+for _ in range(25):
+    a, r = robot.test_update()
+    print("action:", a, "reward:", r)
+    if r == maze.reward["destination"]:
+        print("success")
+        break
+
+
+class Robot(QRobot):
+    valid_action = ['u', 'r', 'd', 'l']
+
+    ''' QLearning parameters'''
+    epsilon0 = 0.5  # 初始贪心算法探索概率
+    gamma = 0.94  # 公式中的 γ
+
+    EveryUpdate = 1  # the interval of target model's updating
+
+    """some parameters of neural network"""
+    target_model = None
+    eval_model = None
+    batch_size = 32
+    learning_rate = 1e-2
+    TAU = 1e-3
+    step = 1  # 记录训练的步数
+
+    """setting the device to train network"""
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
+    def __init__(self, maze):
+        """
+        初始化 Robot 类
+        :param maze:迷宫对象
+        """
+        super(Robot, self).__init__(maze)
+        maze.set_reward(reward={
+            "hit_wall": 10.,
+            "destination": -50.,
+            "default": 1.,
+        })
+        self.maze = maze
+        self.maze_size = maze.maze_size
+
+        """build network"""
+        self.target_model = None
+        self.eval_model = None
+        self._build_network()
+
+        """create the memory to store data"""
+        max_size = max(self.maze_size ** 2 * 3, 1e4)
+        self.memory = ReplayDataSet(max_size=max_size)
+
+    def _build_network(self):
+        seed = 0
+        random.seed(seed)
+
+        """build target model"""
+        self.target_model = QNetwork(state_size=2, action_size=4, seed=seed).to(self.device)
+
+        """build eval model"""
+        self.eval_model = QNetwork(state_size=2, action_size=4, seed=seed).to(self.device)
+
+        """build the optimizer"""
+        self.optimizer = optim.Adam(self.eval_model.parameters(), lr=self.learning_rate)
+
+    def target_replace_op(self):
+        """
+            Soft update the target model parameters.
+            θ_target = τ*θ_local + (1 - τ)*θ_target
+        """
+
+        # for target_param, eval_param in zip(self.target_model.parameters(), self.eval_model.parameters()):
+        #     target_param.data.copy_(self.TAU * eval_param.data + (1.0 - self.TAU) * target_param.data)
+
+        """ replace the whole parameters"""
+        self.target_model.load_state_dict(self.eval_model.state_dict())
+
+    def _choose_action(self, state):
+        state = np.array(state)
+        state = torch.from_numpy(state).float().to(self.device)
+        if random.random() < self.epsilon:
+            action = random.choice(self.valid_action)
+        else:
+            self.eval_model.eval()
+            with torch.no_grad():
+                q_next = self.eval_model(state).cpu().data.numpy()  # use target model choose action
+            self.eval_model.train()
+
+            action = self.valid_action[np.argmin(q_next).item()]
+        return action
+
+    def _learn(self, batch: int = 16):
+        if len(self.memory) < batch:
+            print("the memory data is not enough")
+            return
+        state, action_index, reward, next_state, is_terminal = self.memory.random_sample(batch)
+
+        """ convert the data to tensor type"""
+        state = torch.from_numpy(state).float().to(self.device)
+        action_index = torch.from_numpy(action_index).long().to(self.device)
+        reward = torch.from_numpy(reward).float().to(self.device)
+        next_state = torch.from_numpy(next_state).float().to(self.device)
+        is_terminal = torch.from_numpy(is_terminal).int().to(self.device)
+
+        self.eval_model.train()
+        self.target_model.eval()
+
+        """Get max predicted Q values (for next states) from target model"""
+        Q_targets_next = self.target_model(next_state).detach().min(1)[0].unsqueeze(1)
+
+        """Compute Q targets for current states"""
+        Q_targets = reward + self.gamma * Q_targets_next * (torch.ones_like(is_terminal) - is_terminal)
+
+        """Get expected Q values from local model"""
+        self.optimizer.zero_grad()
+        Q_expected = self.eval_model(state).gather(dim=1, index=action_index)
+
+        """Compute loss"""
+        loss = F.mse_loss(Q_expected, Q_targets)
+        loss_item = loss.item()
+
+        """ Minimize the loss"""
+        loss.backward()
+        self.optimizer.step()
+
+        """copy the weights of eval_model to the target_model"""
+        self.target_replace_op()
+        return loss_item
+
+    def train_update(self):
+        """
+        以训练状态选择动作并更新Deep Q network的相关参数
+        :return :action, reward 如："u", -1
+        """
+        state = self.sense_state()
+        action = self._choose_action(state)
+        reward = self.maze.move_robot(action)
+        next_state = self.sense_state()
+        is_terminal = 1 if next_state == self.maze.destination or next_state == state else 0
+
+        self.memory.add(state, self.valid_action.index(action), reward, next_state, is_terminal)
+
+        """--间隔一段时间更新target network权重--"""
+        if self.step % self.EveryUpdate == 0:
+            self._learn(batch=32)
+
+        """---update the step and epsilon---"""
+        self.step += 1
+        self.epsilon = max(0.01, self.epsilon * 0.995)
+
+        return action, reward
+
+    def test_update(self):
+        """
+        以测试状态选择动作并更新Deep Q network的相关参数
+        :return : action, reward 如："u", -1
+        """
+        state = np.array(self.sense_state(), dtype=np.int16)
+        state = torch.from_numpy(state).float().to(self.device)
+
+        self.eval_model.eval()
+        with torch.no_grad():
+            q_value = self.eval_model(state).cpu().data.numpy()
+
+        action = self.valid_action[np.argmin(q_value).item()]
+        reward = self.maze.move_robot(action)
+        return action, reward
+
+"""  Deep Qlearning 算法相关参数： """
+
+epoch = 10  # 训练轮数
+maze_size = 5  # 迷宫size
+training_per_epoch=int(maze_size * maze_size * 1.5)
+
+""" 使用 DQN 算法训练 """
+
+g = Maze(maze_size=maze_size)
+r = Robot(g)
+runner = Runner(r)
+runner.run_training(epoch, training_per_epoch)
+
+# 生成训练过程的gif图, 建议下载到本地查看；也可以注释该行代码，加快运行速度。
+runner.generate_gif(filename="results/dqn_size10.gif")
+
